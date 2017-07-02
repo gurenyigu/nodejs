818e2cc90bf8152e829d7c2231e0eceba9292f6c
wds <wudongshuo@hotmail.com>
2017-07-02 22:52:14 +0800

    增加图片

--- Node/http/lol.js
+++ Node/http/lol.js
@@ -1,17 +1,26 @@
 const fs = require('fs');
 const http = require('http');
+const query = require('querystring');
 
 http.createServer((req, res) => {
   if (req.url == '/user' && req.method == 'POST') {
-    res.writeHead(200, {
-      'content-Type': 'text/html'
+    let data = '';
+    req.on('data', function(chunk) {
+      data += chunk;
     });
-    try {
-      let content = fs.readFileSync('./user.html', 'utf-8');
-      res.end(content);
-    } catch (err) {
-      error(err);
-    }
+
+    req.on('end', function() {
+      console.log(query.parse(data));
+      res.writeHead(200, {
+        'content-Type': 'text/html'
+      });
+      try {
+        let content = fs.readFileSync('./user.html', 'utf-8');
+        res.end(content);
+      } catch (err) {
+        error(err);
+      }
+    });
   } else if (req.url == '/' && req.method == 'GET') {
     res.writeHead(200, {
       'content-Type': 'text/html'
